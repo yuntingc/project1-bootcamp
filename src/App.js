b4948daab7286a2e20462b7be335fdf722c5c045
neokaiyuan <kai@rocketacademy.co>
2022-05-03 16:54:11 +0800

    Update App component to be class component for easier understanding prior to learning hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import React from "react";
 import logo from "./logo.png";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
